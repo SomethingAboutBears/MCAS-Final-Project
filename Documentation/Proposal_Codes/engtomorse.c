a34b7067452adfeca99cdeaf64cb87544abe8608
SomethingAboutBears <113320823+SomethingAboutBears@users.noreply.github.com>
2023-02-22 23:21:00 +0000

    English to AMC complete

--- Documentation/Proposal_Codes/engtomorse.c
+++ Documentation/Proposal_Codes/engtomorse.c
@@ -2,100 +2,173 @@
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
+//Declare Brute force adder function that returns a unsigned long and takes in an unsigned long
+char* tomorse(char c,char* output);
+
 int main() {
-    char input[1000];
-    printf("Input your text (please use Latin characters with no diacritics and numbers only)");
-    fgets(input, 1000, stdin);
+    char input[2000];
+    printf("Input your text (please use Latin characters with no diacritics and numbers only) \n");
+    fgets(input, 2000, stdin);
     int length = strlen(input);
-    char *output = "";
+    input[length-1] = 0;
+    char output[15000] = "";
     for (int i = 0; i < length; i++) {
         tomorse(input[i],output);
     }
-    printf("%s",output);
+    printf("The AMC as written is: \n %s",output);
     return 0;
 }
 
 char* tomorse(char c,char* output) {
     if (c == 'A' || c == 'a') {
-            strcat(output,".- ");
+        strcat(output,".-   ");
     }
     else if (c == 'B' || c == 'b') {
-            strcat(output,"-... ");
+        strcat(output,"-...   ");
     }
     else if (c == 'C' || c == 'c') {
-            strcat(output,"-.-. ");
+        strcat(output,".. .   ");
     }
     else if (c == 'D' || c == 'd') {
-            strcat(output,"-.. ");
+        strcat(output,"-..   ");
     }
     else if (c == 'E' || c == 'e') {
-            strcat(output,". ");
+        strcat(output,".   ");
     }
     else if (c == 'F' || c == 'f') {
-            strcat(output,"..-. ");
+        strcat(output,".-.   ");
     }
     else if (c == 'G' || c == 'g') {
-            strcat(output,"--. ");
+        strcat(output,"--.   ");
     }
-    else if (c == 'H' || c == 'H') {
-            strcat(output,".... ");
+    else if (c == 'H' || c == 'h') {
+        strcat(output,"....   ");
     }
     else if (c == 'I' || c == 'i') {
-            strcat(output,".. ");
+        strcat(output,"..   ");
     }
     else if (c == 'J' || c == 'j') {
-            strcat(output,".--- ");
+        strcat(output,"-.-.   ");
     }
     else if (c == 'K' || c == 'k') {
-            strcat(output,"-.- ");
+        strcat(output,"-.-   ");
     }
     else if (c == 'L' || c == 'l') {
-            strcat(output,".-.. ");
+        strcat(output,"⸺   ");
     }
     else if (c == 'M' || c == 'm') {
-            strcat(output,"-- ");
+        strcat(output,"--   ");
     }
     else if (c == 'N' || c == 'n') {
-            strcat(output,"-. ");
+        strcat(output,"-.   ");
     }
     else if (c == 'O' || c == 'o') {
-            strcat(output,"--- ");
+        strcat(output,". .   ");
     }
     else if (c == 'P' || c == 'p') {
-            strcat(output,".--. ");
+        strcat(output,".....   ");
     }
     else if (c == 'Q' || c == 'q') {
-            strcat(output,"--.- ");
+        strcat(output,"..-.   ");
     }
     else if (c == 'R' || c == 'r') {
-            strcat(output,".-. ");
+        strcat(output,". ..   ");
     }
     else if (c == 'S' || c == 's') {
-            strcat(output,"... ");
+        strcat(output,"...   ");
     }
     else if (c == 'T' || c == 't') {
-            strcat(output,"- ");
+        strcat(output,"-   ");
     }
     else if (c == 'U' || c == 'u') {
-            strcat(output,"..- ");
+        strcat(output,"..-   ");
     }
     else if (c == 'V' || c == 'v') {
-            strcat(output,"...- ");
+        strcat(output,"...-   ");
     }
     else if (c == 'W' || c == 'w') {
-            strcat(output,".-- ");
+        strcat(output,".--   ");
     }
     else if (c == 'X' || c == 'x') {
-            strcat(output,"-..- ");
+        strcat(output,".-..   ");
     }
     else if (c == 'Y' || c == 'y') {
-            strcat(output,"-.-- ");
+        strcat(output,".. ..   ");
     }
     else if (c == 'Z' || c == 'z') {
-            strcat(output,"--.. ");
+        strcat(output,"... .   ");
     }
-        else if (c= ' ') {
-            strcat(output,"/ ");
-        }
-        return *output;
+    else if (c == '0') {
+        strcat(output,"⸻   ");
+    }
+    else if (c == '1') {
+        strcat(output,".--.   ");
+    }
+    else if (c == '2') {
+        strcat(output,"..-..   ");
+    }
+    else if (c == '3') {
+        strcat(output,"...-.   ");
+    }
+    else if (c == '4') {
+        strcat(output,"....-   ");
+    }
+    else if (c == '5') {
+        strcat(output,"---   ");
+    }
+    else if (c == '6') {
+        strcat(output,"......   ");
+    }
+    else if (c == '7') {
+        strcat(output,"--..   ");
+    }
+    else if (c == '8') {
+        strcat(output,"-....   ");
+    }
+    else if (c == '9') {
+        strcat(output,"-..-   ");
+    }
+    else if (c == '.') {
+        strcat(output,"..--..   ");
+    }
+    else if (c == ',') {
+        strcat(output,".-.-   ");
+    }
+    else if (c == ':') {
+        strcat(output,"-.- . .   ");
+    }
+    else if (c == '?') {
+        strcat(output,"-..-.   ");
+    }
+    else if (c == '\'') {
+        strcat(output,"..-. .-..   ");
+    }
+    else if (c == '-') {
+        strcat(output,"... .-..   ");
+    }
+    else if (c == '/') {
+        strcat(output,"..- -   ");
+    }
+    else if (c == '(') {
+        strcat(output,"..... -.   ");
+    }
+    else if (c == ')') {
+        strcat(output,"..... .. ..   ");
+    }
+    else if (c == '"') {
+        strcat(output,"..-. -.   ");
+    }
+    else if (c == '&') {
+        strcat(output,". ...   ");
+    }
+    else if (c == '!') {
+        strcat(output,"---.   ");
+    }
+    else if (c == ';') {
+        strcat(output,"... ..   ");
+    }
+    else if (c= ' ') {
+        strcat(output,"/   ");
+    }
+    return output;
     }
